8e5b4cd28a22a5234a796765494b841c11b19995
Carl Duncan <cdsoftwaresja@gmail.com>
2021-07-17 02:24:02 -0500

    Minor Changes to the login page structure

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -10,105 +10,110 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: CupertinoNavigationBar(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         border: null,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(
-                    bottom: size.height * 0.05, top: size.height * 0.05),
-                child: CircleAvatar(
-                  radius: size.height * 0.1,
-                  backgroundColor: Constant.primary,
+      body: loadUI(context),
+    );
+  }
+
+  loadUI(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                  bottom: size.height * 0.05, top: size.height * 0.05),
+              child: CircleAvatar(
+                radius: size.height * 0.1,
+                backgroundColor: Constant.primary,
+              ),
+            ),
+            Text(
+              "[Tag Line here]",
+              style: Theme.of(context).textTheme.headline6,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et sodales risus. Integer finibus dui diam, molestie varius enim elementum posuere.",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText2
+                  .copyWith(color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Field(
+                hint: "Email Address",
+                controller: null,
+                obscure: false,
+                suffix: Icons.email_outlined,
+                label: null,
+                enabled: true),
+            Field(
+                hint: "Password",
+                controller: null,
+                obscure: true,
+                suffix: Icons.password,
+                label: null,
+                enabled: true),
+            IconButtonWidget(
+                buttonText: "Sign in",
+                buttonColor: Constant.primary,
+                onPressed: () {},
+                icon: Icon(
+                  AntDesign.login,
+                )),
+            Row(
+              children: [
+                Expanded(
+                  child: IconButtonWidget(
+                      buttonText: "Sign in Apple",
+                      buttonColor: Colors.black,
+                      onPressed: () {},
+                      icon: Icon(FontAwesome5Brands.apple)),
                 ),
-              ),
-              Text(
-                "[Tag Line here]",
-                style: Theme.of(context).textTheme.headline6,
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et sodales risus. Integer finibus dui diam, molestie varius enim elementum posuere.",
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText2
-                    .copyWith(color: Colors.grey),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Field(
-                  hint: "Email Address",
-                  controller: null,
-                  obscure: false,
-                  suffix: Icons.email_outlined,
-                  label: null,
-                  enabled: true),
-              Field(
-                  hint: "Password",
-                  controller: null,
-                  obscure: true,
-                  suffix: Icons.password,
-                  label: null,
-                  enabled: true),
-              IconButtonWidget(
-                  buttonText: "Sign in",
-                  buttonColor: Constant.primary,
-                  onPressed: () {},
-                  icon: Icon(
-                    AntDesign.login,
-                  )),
-              Row(
-                children: [
-                  Expanded(
-                    child: IconButtonWidget(
-                        buttonText: "Sign in Apple",
-                        buttonColor: Colors.black,
-                        onPressed: () {},
-                        icon: Icon(FontAwesome5Brands.apple)),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    child: IconButtonWidget(
-                        buttonText: "Sign in Google",
-                        buttonColor: Colors.black,
-                        onPressed: () {},
-                        icon: Icon(FontAwesome5Brands.google)),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Dont have an account? "),
-                  Text(
-                    "Click here!",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText2
-                        .copyWith(color: Constant.primary),
-                  )
-                ],
-              )
-            ],
-          ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: IconButtonWidget(
+                      buttonText: "Sign in Google",
+                      buttonColor: Colors.black,
+                      onPressed: () {},
+                      icon: Icon(FontAwesome5Brands.google)),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Dont have an account? "),
+                Text(
+                  "Click here!",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(color: Constant.primary),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
