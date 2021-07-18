95223cdd82b90ed89e121b25a320b7092937728c
Carl Duncan <cdsoftwaresja@gmail.com>
2021-07-17 21:38:39 -0500

    Smoothed out the category hero animation

--- lib/pages/customer/category_view/category_view.dart
+++ lib/pages/customer/category_view/category_view.dart
@@ -17,14 +17,6 @@ class _CategoryViewState extends State<CategoryView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CupertinoNavigationBar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        middle: Text(
-          "Purchase ${widget.category.name} Card",
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        border: null,
-      ),
       body: StreamBuilder(
           stream: null,
           builder: (context, snapshot) {
@@ -38,17 +30,41 @@ class _CategoryViewState extends State<CategoryView> {
   loadUI() {
     return CustomScrollView(
       slivers: [
+        SliverAppBar(
+          expandedHeight: 150.0,
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          pinned: false,
+          floating: false,
+          flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(widget.category.name,
+                  style: Theme.of(context).textTheme.headline6),
+              background: Hero(
+                tag: widget.category.id,
+                child: Container(
+                  child: ShaderMask(
+                      shaderCallback: (rect) {
+                        return LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Constant.primary, Colors.transparent],
+                        ).createShader(
+                            Rect.fromLTRB(0, 0, rect.width, rect.height));
+                      },
+                      blendMode: BlendMode.dstIn,
+                      child: Container(
+                        child: CachedNetworkImage(
+                          imageUrl: Repository.s3 + widget.category.image,
+                          fit: BoxFit.cover,
+                        ),
+                      )),
+                ),
+              )),
+        ),
         SliverPadding(
           padding: Constant.padding,
-          sliver: SliverList(
-              delegate: SliverChildListDelegate.fixed([
-            Hero(
-              tag: widget.category.id,
-              child: Container(
-                child: CachedNetworkImage(imageUrl: Repository.s3 + widget.category.image,),
-              ),
-            )
-          ])),
+          sliver: SliverList(delegate: SliverChildListDelegate.fixed([])),
         )
       ],
     );
