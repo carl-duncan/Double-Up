52dab4b7e3bc3336c4fef947d637dd8f3184a414
Carl Duncan <cdsoftwaresja@gmail.com>
2021-07-20 05:09:45 -0500

    Added check if the QR code has already been redeemed

--- lambda_functions/calculate-discount.py
+++ lambda_functions/calculate-discount.py
@@ -6,14 +6,20 @@ from decimal import Decimal
 
 def lambda_handler(event, context):
     value = calulateTotalDiscount(event["id"])
-    updateUserPoints(event["user"],value)
+    if (value["redeemed"]!=True):
+        value["redeemed"]=True
+        dynamodb = boto3.resource('dynamodb')
+
+        setRedeemed(event["id"],dynamodb)
+        updateUserPoints(event["user"],value["price"],dynamodb)
+    else:
+        value["redeemed"]="Already Redeemed/Invalid QR"
     return {
         'statusCode': 200,
         'body':value
     }
 
-def updateUserPoints(id,amount):
-    dynamodb = boto3.resource('dynamodb')
+def updateUserPoints(id,amount,dynamodb):
     table = dynamodb.Table('CustomerTable')
     response = table.update_item(
         Key={
@@ -26,9 +32,23 @@ def updateUserPoints(id,amount):
         ReturnValues="UPDATED_NEW"
     )
 
+def setRedeemed(transaction_id,dynamodb):
+    table = dynamodb.Table('TransactionTable')
+
+    response = table.update_item(
+        Key={
+            'id': transaction_id,
+        },
+        UpdateExpression="set redeemed = :val",
+        ExpressionAttributeValues={
+            ':val': True
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
 def calulateTotalDiscount(id):
     url = ""
-    payload = "{\"query\":\"query MyQuery {\\n  getTransaction(id: \\\""+id+"\\\") {\\n    id\\n    products {\\n      loss\\n      min\\n      max\\n      price\\n      sales\\n    }\\n  }\\n}\\n\",\"operationName\":\"MyQuery\"}"
+    payload = "{\"query\":\"query MyQuery {\\n  getTransaction(id: \\\""+id+"\\\") {\\n    id\\n  redeemed\\n  products {\\n      loss\\n      min\\n      max\\n      price\\n      sales\\n    }\\n  }\\n}\\n\",\"operationName\":\"MyQuery\"}"
     http = urllib3.PoolManager()
     response = http.request("POST", url, body=payload,headers={'Content-Type': 'application/json',"x-api-key": ""})
     resp_json=json.loads(response.data.decode('utf8'))
@@ -41,7 +61,7 @@ def calulateTotalDiscount(id):
         price_total+=products[x]["price"]
         # print(value)
         total+=value
-    return price_total * (total/length)
+    return {"price": price_total * (total/length),"redeemed":resp_json["data"]["getTransaction"]["redeemed"]}
 
 def discount(UnsoldCount, SalesCount, MinDiscount, MaxDiscount):
     return max(min(UnsoldCount/SalesCount, MaxDiscount), MinDiscount)
