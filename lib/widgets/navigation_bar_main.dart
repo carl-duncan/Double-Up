25ccef9722bc0f41cd4e84c5d996c0f7c31a7a70
Carl Duncan <cdsoftwaresja@gmail.com>
2021-07-24 09:09:10 -0500

    Minor UI cahnges

--- lib/widgets/navigation_bar_main.dart
+++ lib/widgets/navigation_bar_main.dart
@@ -55,9 +55,15 @@ navigationBarPushed(BuildContext context, String title) {
   return SliverList(
       delegate: SliverChildListDelegate.fixed([
     CupertinoNavigationBar(
-      leading: InkWell(child: Icon(CupertinoIcons.back),onTap: (){
-        Navigator.pop(context);
-      },),
+      leading: InkWell(
+        child: Icon(
+          CupertinoIcons.back,
+          size: 25,
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       middle: Text(
         title,
